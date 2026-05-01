b8e59374bc7fee8ce20c21c7dba29f999f00c4e0
Tuan Todd Cao <caonhattuan@gmail.com>
2026-05-02 01:45:32 +0700

    2026

---
+++ code_to_push.R
@@ -0,0 +1,17 @@
+#Code first time
+git remote add origin https://github.com/justtoddcao/tuantoddcao.github.io.git
+git branch -M main
+git add --all
+git commit -m "first commit"
+git push -u origin main
+
+git config --global user.name "Tuan Todd Cao"
+git config --global user.email "caonhattuan@gmail.com"
+
+
+## Updating
+
+quarto render
+git add --all
+git commit -m "update"
+git push
